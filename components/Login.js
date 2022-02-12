17cd600b0c41c487d6387311a8b5064359209316
Michael <Mlcorrieri@gmail.com>
2022-02-12 12:47:03 -0500

    login built

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,55 @@
+import React, { Component, useState } from "react";
+import { Text, View, TextInput, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Say HELLO to Duey</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={styles.text}>TriHealth</Text>
+      <TextInput
+        style={styles.inputs}
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      ></TextInput>
+      <TextInput style={styles.inputs} placeholder="Password"></TextInput>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputs: {
+    width: 240,
+    marginHorizontal: 20,
+    padding: 10,
+    backgroundColor: "lightblue",
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  text: {
+    textAlign: "center",
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 5,
+    borderRadius: 5,
+    alignItems: "center",
+    textAlign: "center",
+    width: 70,
+    marginLeft: 100,
+  },
+  buttonText: {
+    fontSize: 14,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
